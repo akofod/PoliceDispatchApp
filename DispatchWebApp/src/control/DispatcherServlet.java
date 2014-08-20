0cb0694f602197d26e81d88f9db14a42d57df7f6
William Kofod <kofod01@email.franklin.edu>
2014-08-20 05:56:44 -0400

    Created DispatcherServlet to handle actions, instantiate Controller, and forward proper RequestDispatcher.

---
+++ DispatchWebApp/src/control/DispatcherServlet.java
@@ -0,0 +1,58 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DispatcherServlet
+ */
+@WebServlet("/DispatcherServlet")
+public class DispatcherServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DispatcherServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+	
+	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Get the action from the request
+		String uri = request.getRequestURI();
+		int lastIndex = uri.lastIndexOf("/");
+		String action = uri.substring(lastIndex + 1);
+		
+		// Find the correct action, instantiate the Controller and set the dispatch URL
+		String dispatchURL = null;
+		
+		//TODO: insert if/else statements for Controller classes
+		
+		// Forward request dispatcher
+		if (dispatchURL != null) {
+			RequestDispatcher rd = request.getRequestDispatcher(dispatchURL);
+			rd.forward(request, response);
+		}
+	}
+
+}
